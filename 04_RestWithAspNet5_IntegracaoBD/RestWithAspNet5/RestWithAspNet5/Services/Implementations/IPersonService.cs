88cdb4f2da28570b39594ba25432fff9faa3a991
Nayton Batista <naytonbatista@hotmail.com>
2020-12-03 14:57:12 -0400

    Integração com Banco de Dados

---
+++ 04_RestWithAspNet5_IntegracaoBD/RestWithAspNet5/RestWithAspNet5/Services/Implementations/IPersonService.cs
@@ -0,0 +1,20 @@
+﻿using RestWithAspNet5.Controllers.Model;
+using System.Collections.Generic;
+
+namespace RestWithAspNet5.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+
+    }
+}
