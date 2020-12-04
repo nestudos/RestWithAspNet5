0123515ac151ef2b9413202d3140f701430046ba
Nayton Batista <naytonbatista@hotmail.com>
2020-12-04 13:47:09 -0400

    Versionamento da api

---
+++ 05_RestWithAspNet5_VersionamentoAPI/RestWithAspNet5/RestWithAspNet5/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,107 @@
+﻿using RestWithAspNet5.Controllers.Model;
+using RestWithAspNet5.Controllers.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet5.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private MySqlContext _context;
+
+        public PersonServiceImplementation(MySqlContext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Person> FindAll()
+        {
+
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id == id);
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id == id);
+
+            if (result == null)
+            {
+                return;
+            }
+
+            try
+            {
+                _context.Persons.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id))
+            {
+                return new Person();
+            }
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id == person.Id);
+
+            if (result == null)
+            {
+                return person;
+            }
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return person;
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id == id);
+        }
+    }
+}
