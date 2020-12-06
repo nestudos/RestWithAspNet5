c4ec9118f9e30c74f7813a317800f773dd63b3b9
Nayton Batista <naytonbatista@hotmail.com>
2020-12-06 14:59:04 -0400

    Adicionando arquitetura em camadas

---
+++ 06_RestWithAspNet5_Arquitetura/RestWithAspNet5/RestWithAspNet5/Repository/Implementations/IPersonRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using RestWithAspNet5.Model;
+using System.Collections.Generic;
+
+namespace RestWithAspNet5.Repository.Implementations
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+
+    }
+}
---
+++ 06_RestWithAspNet5_Arquitetura/RestWithAspNet5/RestWithAspNet5/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,105 @@
+﻿using RestWithAspNet5.Model.Context;
+using RestWithAspNet5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithAspNet5.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        private MySqlContext _context;
+
+        public PersonRepositoryImplementation(MySqlContext context)
+        {
+            _context = context;
+
+        }
+
+        public List<Person> FindAll()
+        {
+
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id == id);
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id == id);
+
+            if (result == null)
+            {
+                return;
+            }
+
+            try
+            {
+                _context.Persons.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id))
+            {
+                return new Person();
+            }
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id == person.Id);
+
+            if (result == null)
+            {
+                return person;
+            }
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+            return person;
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id == id);
+        }
+    }
+}
