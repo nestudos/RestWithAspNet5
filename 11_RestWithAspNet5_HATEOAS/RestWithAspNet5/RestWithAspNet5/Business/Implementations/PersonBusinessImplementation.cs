17a44c1c8c6a6ceeab04a9b3ddba057174a09586
Nayton Batista <naytonbatista@hotmail.com>
2020-12-07 15:00:09 -0400

    Implemenando HATEOAS

---
+++ 11_RestWithAspNet5_HATEOAS/RestWithAspNet5/RestWithAspNet5/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,67 @@
+﻿using RestWithAspNet5.Model;
+using System;
+using System.Collections.Generic;
+using RestWithAspNet5.Repository;
+using RestWithAspNet5.Data.VO;
+using RestWithAspNet5.Data.Converter.Implementations;
+
+namespace RestWithAspNet5.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        private readonly PersonConverter _converter;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _converter = new PersonConverter();
+
+        }
+
+        public List<PersonVO> FindAll()
+        {
+
+            return _converter.Parse(_repository.FindAll());
+        }
+
+        public PersonVO FindById(long id)
+        {
+            return _converter.Parse(_repository.FindById(id));
+        }
+
+        public PersonVO Create(PersonVO person)
+        {
+            try
+            {
+                var personEntity = _converter.Parse(person);
+
+                personEntity =  _repository.Create(personEntity);
+                return _converter.Parse(personEntity);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public PersonVO Update(PersonVO person)
+        {
+            var personEntity = _converter.Parse(person);
+
+            personEntity = _repository.Update(personEntity);
+            return _converter.Parse(personEntity);
+        }
+                
+    }
+}
---
+++ 11_RestWithAspNet5_HATEOAS/RestWithAspNet5/RestWithAspNet5/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using RestWithAspNet5.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithAspNet5.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
