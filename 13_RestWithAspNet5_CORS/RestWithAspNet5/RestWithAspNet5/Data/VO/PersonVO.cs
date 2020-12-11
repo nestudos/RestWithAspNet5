d385d2f77f00269fd9602ab6e5e83f179ccc7277
Nayton Batista <naytonbatista@hotmail.com>
2020-12-11 01:41:24 -0400

    Habilitando CORS

---
+++ 13_RestWithAspNet5_CORS/RestWithAspNet5/RestWithAspNet5/Data/VO/PersonVO.cs
@@ -0,0 +1,23 @@
+﻿using RestWithAspNet5.Hypermedia;
+using RestWithAspNet5.Hypermedia.Abstract;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestWithAspNet5.Data.VO
+{
+    public class PersonVO : ISupportHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [JsonIgnore]
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
---
+++ 13_RestWithAspNet5_CORS/RestWithAspNet5/RestWithAspNet5/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithAspNet5.Model.Base;
+using RestWithAspNet5.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithAspNet5.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        private MySqlContext _context;
+
+        private DbSet<T> _dataset;
+        public GenericRepository(MySqlContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                _dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(long id)
+        {
+            var result = _dataset.SingleOrDefault(p => p.Id == id);
+
+            if (result == null)
+            {
+                return;
+            }
+
+            try
+            {
+                _dataset.Remove(result);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public List<T> FindAll()
+        {
+            return _dataset.ToList();
+        }
+
+        public T FindById(long id)
+        {
+            return _dataset.SingleOrDefault(p => p.Id == id);
+        }
+
+        public T Update(T item)
+        {
+            var result = _dataset.SingleOrDefault(p => p.Id == item.Id);
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(item);
+                _context.SaveChanges();
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool Exists(long id)
+        {
+            return _dataset.Any(p => p.Id == id);
+        }
+    }
+}
