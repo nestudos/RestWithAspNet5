776c2b89fd20db2272dbefad36078c08dcef3a54
Nayton Batista <naytonbatista@hotmail.com>
2020-12-23 19:37:37 -0400

    Implementando autenticação

---
+++ 14_RestWithAspNet5_Autenticacao/RestWithAspNet5/RestWithAspNet5/Repository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using RestWithAspNet5.Data.VO;
+using RestWithAspNet5.Model;
+
+namespace RestWithAspNet5.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);
+
+        User ValidateCredentials(string userName);
+
+
+        User RefreshUserInfo(User user);
+
+        bool RevokeToken(string userName);
+    }
+}
